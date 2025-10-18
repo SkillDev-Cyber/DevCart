f8ce8eda063414a867cde63daf03c41194746388
SkillDev-Cyber <153659337+SkillDev-Cyber@users.noreply.github.com>
2025-10-18 11:34:15 +0330

    Projects & Latest Articles Moved to view Component

---
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Models.Project>
+            {
+                new Models.Project(1, "تاکسی", "درخواست آنلاین تاکسی برای سفرهای درون شهری","project-1.jpg", "Snapp"),
+                new Models.Project(2, "زودفود", "درخواست آنلاین غذا برای سراسر کشور","project-2.jpg", "Zoodfood"),
+                new Models.Project(3, "مدارس", "سیستم مدیریت یکپارچه مدارس","project-3.jpg", "Monop"),
+                new Models.Project(4, "فضاپیما", "برنامه مدیریت فضاپیماهای ناسا","project-4.jpg", "Nasa"),
+            };
+            return View("_Projects", projects);
+        }
+    }
+}
